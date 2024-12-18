812d9f525796aef8fc5e974bb6841684eb01081b
Mert Çakır <Mertckr17@outlook.com>
2024-12-18 17:23:24 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const totalPrice = quantity * pricePerDroid;
-
-    if(totalPrice >customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
         return "Insufficient funds!";
-    } else { 
-        return `You ordered ${quantity} droids worth ${totalPrice} credits. Remaining credits: ${remainingCredits}`;
     }
-    
+
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000));// "You ordered 5 droids worth 150000 credits"
